e0ff83397166367dc8679942a4af9a61f457f3a3
yuan442177 <15973370670@163.com>
2017-10-31 17:07:05 +0800

    导航栏,页脚,搜索表单,轮播图静态页面

--- auction/src/app/app.module.ts
+++ auction/src/app/app.module.ts
@@ -2,12 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';
 
 @NgModule({
-  declarations: [//声明模块中有什么东西 组件,指令,管道
-    AppComponent
+  //声明模块中有什么东西 组件,指令,管道
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent,
+    ProductComponent,
+    StarsComponent
   ],
-  imports: [//声明模块所需的其他模块
+  //声明模块所需的其他模块
+  imports: [
     BrowserModule
   ],
   providers: [],//声明服务
